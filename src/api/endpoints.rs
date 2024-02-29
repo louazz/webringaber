e19101ed09521e2d97490ee110acea4b3060d1ed
louazz <louaizaiter@gmail.com>
2024-02-29 13:13:50 +0000

    change

--- src/api/endpoints.rs
+++ src/api/endpoints.rs
@@ -1,18 +1,34 @@
 use actix_web::{web, HttpRequest, HttpResponse, Responder};
 use serde::Deserialize;
 
+use crate::site_indexer::AppState;
+
 #[derive(Debug, Deserialize)]
 pub struct Params {
     dir: String,
 }
 
-pub async fn navigate(req: HttpRequest, path: web::Path<String>) -> impl Responder {
+pub async fn navigate(req: HttpRequest, path: web::Path<String>, data: web::Data<AppState>) -> impl Responder {
     let user_id = path.into_inner();
+    println!("{}",user_id);
+    // check if user id is available on the server
+    println!("{}", data.sites[0].0);
 
-    if let Ok(params) = web::Query::<Params>::from_query(req.query_string()) {
-        web::Redirect::to("https://longdogechallenge.com")
-    } else {
+    let mut found = false;
+    let mut item_url="";
+    for item in data.sites.iter(){
+       if item.0 == user_id {
+        found = true;
+        item_url = &item.1;
+        break;
+       }
+    }
+    if found == true {
+        web::Redirect::to(item_url.to_string())
+
+    }else{
         web::Redirect::to("/invalid_dir")
+
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,20 @@ mod site_indexer;
 use actix_web::{self, web, App, HttpServer};
 use api::endpoints::*;
 use site_indexer::AppState;
+use site_indexer::read_file;
+use std::path::Path;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    println!("Server starting ..");
+    let path = Path::new("/home/louai/aber_webring/list.txt");
+    let data =read_file(path);
+    HttpServer::new(move || {
         App::new()
-            .route("/{user_id}", web::get().to(invalid_dir_error))
-            .route("/invalid_dir", web::get().to(navigate))
+            .route("/user/{user_id}", web::get().to(navigate))
+            .route("/invalid_dir", web::get().to(invalid_dir_error))
             .app_data(web::Data::new(AppState {
-                sites: vec![("user_id".to_string(), "url".to_string())],
+                sites: data.clone(),
             }))
     })
     .bind(("127.0.0.1", 8080))?
